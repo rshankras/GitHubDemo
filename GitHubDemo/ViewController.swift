614db0435c0b78fc86310a5e541b5b57926d39da
Ravi Shankar <ravishankar@outlook.in>
2016-06-27 16:01:32 -0700

    Removed World
    Removed World

--- GitHubDemo/ViewController.swift
+++ GitHubDemo/ViewController.swift
@@ -13,7 +13,7 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        print("Hello World")
+        print("Hello")
     }
 
     override func didReceiveMemoryWarning() {
